ed59921aea7d729433373b41cf664c5fc7d2603a
Makasya <maxytka228@gmail.com>
2025-10-14 23:13:21 +0500

    task7 lib

--- Tyuiu.FisherMA.Sprint5.Task7.V15.Lib/DataService.cs
+++ Tyuiu.FisherMA.Sprint5.Task7.V15.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using tyuiu.cources.programming.interfaces.Sprint5;
 
 namespace Tyuiu.FisherMA.Sprint5.Task7.V15.Lib
@@ -10,19 +10,11 @@ namespace Tyuiu.FisherMA.Sprint5.Task7.V15.Lib
     {
         public string DeleteWordsOfLengthTwo(string pathInput, string pathOutput)
         {
-          
             string text = File.ReadAllText(pathInput, Encoding.UTF8);
-
-            char[] separators = { ' ', '\t', '\r', '\n', ',', '.', ';', ':', '!', '?', '-', '(', ')', '"' };
-
-            var words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
-            var filtered = words.Where(w => w.Length != 2).ToList();
-
-            string result = string.Join(" ", filtered);
-
+            string pattern = @"\b[А-Яа-яA-Za-z]{2}\b";
+            string result = Regex.Replace(text, pattern, "", RegexOptions.Multiline);
+            result = Regex.Replace(result, @"\s{2,}", " ").Trim();
             File.WriteAllText(pathOutput, result, Encoding.UTF8);
-
             return result;
         }
 
