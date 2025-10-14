1732c30bd66a60d7fd67fd7e68ae042a629e4968
Makasya <maxytka228@gmail.com>
2025-10-14 20:48:50 +0500

    task5

--- Tyuiu.FisherMA.Sprint5.Task5.V6.Lib/DataService.cs
+++ Tyuiu.FisherMA.Sprint5.Task5.V6.Lib/DataService.cs
@@ -10,17 +10,17 @@ namespace Tyuiu.FisherMA.Sprint5.Task5.V6.Lib
     {
         public double CalculateAverage(string path)
         {
-            string[] lines = File.ReadAllLines(path);
+            string line = File.ReadAllText(path);
 
-            double[] numbers = lines
-                .SelectMany(l => l.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries))
+            double[] numbers = line
+                .Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(s => double.Parse(s, CultureInfo.InvariantCulture))
                 .ToArray();
 
             if (numbers.Length == 0)
                 throw new InvalidOperationException("Файл не содержит чисел.");
 
-            double average = numbers.Select(n => Math.Abs(n)).Average();
+            double average = numbers.Average();
 
             return Math.Round(average, 3);
         }
